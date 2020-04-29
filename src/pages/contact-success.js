ab2e66a40d9f50465fcb3314da194927b812aa73
riku0329 user name <riku03290@gmail.com>
2020-04-29 19:45:18 +0900

    edit contact-success h3 styled

--- src/pages/contact-success.js
+++ src/pages/contact-success.js
@@ -1,13 +1,30 @@
 import React from "react"
 import Layout from "../components/layout"
+import styled from "styled-components"
+import { Link } from "gatsby"
+
+
+const ContactSuccessWrapper = styled.section`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  a{
+    text-decoration: none;
+  }
+`
 
 
 const ContactSuccess = () => {
   return (
     <Layout>
-      <h1>
-        送信されました。
-      </h1>
+      <ContactSuccessWrapper>
+        <h3>
+          メッセージが送信されました。折り返しご連絡いたします。
+        </h3>
+        <Link to="/">トップに戻る</Link>
+      </ContactSuccessWrapper>
     </Layout>
   )
 }
